17e6a4048e280905442eaaa592b18b5105a4a87d
Diam177 <92233992+Diam177@users.noreply.github.com>
2025-09-10 19:30:20 +0200

    Update intraday_chart.py

--- lib/intraday_chart.py
+++ lib/intraday_chart.py
@@ -369,14 +369,14 @@ def render_key_levels_section(ticker: str, rapid_host: Optional[str], rapid_key:
         plot_bgcolor="#161B22", paper_bgcolor="#161B22",
         font=dict(color="white"), template=None
     )
-    fig.update_xaxes(range=[tickvals[0], tickvals[-1]], fixedrange=True, tickmode="array", tickvals=tickvals, ticktext=ticktext, tickfont=dict(size=12))
+    fig.update_xaxes(range=[tickvals[0], tickvals[-1]], fixedrange=True, tickmode="array", tickvals=tickvals, ticktext=ticktext, tickfont=dict(size=10))
     fig.update_yaxes(
         fixedrange=True,
         range=(y_range if y_range is not None else None),
         tickmode=('array' if y_tickvals is not None else 'auto'),
         tickvals=(y_tickvals if y_tickvals is not None else None),
         ticktext=([str(v) for v in y_tickvals] if y_tickvals is not None else None),
-        tickfont=dict(size=12)
+        tickfont=dict(size=10)
     )
     # Дата под осью
     try:
