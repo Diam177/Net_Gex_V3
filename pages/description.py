164787d04bf3cf66d7930731ef1b4f87a8dbdded
Diam177 <92233992+Diam177@users.noreply.github.com>
2025-09-14 15:34:39 +0200

    Add files via upload

---
+++ pages/description.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.set_page_config(page_title="Description", layout="wide")
+st.title("Description")
+st.info("Страница-заглушка для описания проекта. Добавьте здесь текст позже.")
